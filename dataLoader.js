f0b067358d0ebddf1451861ee48751071c081e01
Kevin Li <cyberpatriot49169@gmail.com>
2023-01-08 11:22:40 -0500

    Redesign

--- dataLoader.js
+++ dataLoader.js
@@ -6,15 +6,23 @@ fetch("clubs.json")
    let placeholder = document.querySelector("#grid");
    let out = "";
    for(let club of clubs){
+      //<div class="card_image"> <img src=${club.icon} /></div>
       out += `
         <div class="card 1" data-category="Arts" onclick = "showInfo()">
-            <div class="card_image"> <img src=${club.icon} /></div>
-            <div class="card_title title-white">
-                <p>${club.club}</p>
-            </div>
             
-        </div>
+            <div class="card_title title-white"><p>${club.club}</p></div>
+            <div class="card_icon"> <img src=${club.icon}></div>
+            <div class="tag_container">
       `;
+      if(club.subtags){
+         const tagsList = club.subtags.split(", ");
+         for(let tag of tagsList){
+            out += `<div class="card_tag">${tag}</div>`;
+         }
+      }
+      
+      out+= `</div></div>`;
+
    }
  
    placeholder.innerHTML = out;
